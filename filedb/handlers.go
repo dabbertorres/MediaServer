27535b9d580479d3d2ba50d79115c045cda93cc4
Alec Iverson <adiverso93@gmail.com>
2017-06-04 18:02:23 -0700

    actual progress on the database server

---
+++ filedb/handlers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"MediaServer/internal/msg/search"
+)
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	data, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+
+	searchParams := search.Request{}
+	if err := json.Unmarshal(data, &searchParams); err != nil {
+		log.Printf("Bad search request: '%s'\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	
+}
+
+func streamHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- filedb/main.go
+++ filedb/main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
+	"MediaServer/internal/connect"
+	"MediaServer/internal/interrupt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db := openDB()
+	defer db.Close()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/search", searchHandler)
+	mux.HandleFunc("/stream", streamHandler)
+
+	server := http.Server{
+		Addr:           ":" + connect.DatabasePort,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 32,
+		Handler:        mux,
+	}
+
+	// listen for termination signal!
+	interrupt.OnExit(func() { server.Close() })
+
+	fmt.Println("Serving...")
+	err := server.ListenAndServe()
+	if err == http.ErrServerClosed {
+		fmt.Println("Done")
+	} else {
+		log.Println("Server shutdown unexpectedly:", err)
+	}
+}
+
+func openDB() *sql.DB {
+	db, err := sql.Open("mysql", "filedb@/media")
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
